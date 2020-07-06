c698d52b9928cd3fee08bde3f25720a7e37631bb
bzubaid <bzubaid@ibm.com>
2020-07-06 11:50:01 -0400

    feat(tutorial): complete step 3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'core-js/modules/es6.array.fill';
 import 'core-js/modules/es6.string.includes';
 import 'core-js/modules/es6.string.trim';
 import 'core-js/modules/es7.object.values';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
@@ -14,7 +13,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
-  uri: 'https://api.github.com/graph1',
+  uri: 'https://api.github.com/graphql',
   headers: {
     authorization: `Bearer ${
       process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
